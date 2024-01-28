c014da4fa6d3097dd26fd0a51c6a3bf9fdadfba0
ClarenceLaria <clarencelaria@gmail.com>
2024-01-28 16:28:22 +0300

    updated Auth page

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,12 @@ import AuthForm from "./(ui)/AuthForm";
 export default function Home() {
   return (
     <>
-      <div className="w-screen h-screen  flex ">
-        <div className="w-1/2 ">
-          <Image src={student} alt="Students" className=""/>
+      <div className="w-screen h-screen  flex bg-gray-100">
+        <div className="w-full h-full hidden sm:block ">
+          <Image src={student} alt="Students" className="w-full h-full object-cover"/>
         </div>
-        <div className="w-1/2 bg-gray-500 flex items-center justify-center flex-col">
-          <Image src={logo} alt="logo" className=""/>
+        <div className="w-full h-full flex flex-col items-center justify-center">
+          <Image src={logo} alt="logo" className="w-20 h-20"/>
           <h2>Sign in Now</h2>
           <div className="mx-auto">
             <AuthForm></AuthForm>
