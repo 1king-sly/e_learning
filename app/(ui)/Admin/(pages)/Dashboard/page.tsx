ba46f3a0397da23c71b7940febf408f950574094
Byrone Kinsly <Kinslybyrone17@gmail.com>
2024-01-28 18:44:27 +0300

    restructured files

--- app/(ui)/Admin/(pages)/Dashboard/page.tsx
+++ app/(ui)/Admin/(pages)/Dashboard/page.tsx
@@ -1,18 +1,7 @@
-// import React, { Suspense } from 'react'
-// import Cards from '../../Component/Cards'
-// import Tables from '../../Component/Tables'
-// import Dashboard from '../../Skeleton/Dashboard'
+import React from 'react'
 
-// export default function page() {
-//   return (
-//   <>
-//   <Suspense fallback={<Dashboard></Dashboard>}>
-
-// <Cards></Cards>
-// <Tables></Tables>
-
-// </Suspense>
-  
-//   </> 
-//  )
-// }
+export default function page() {
+  return (
+    <div>page</div>
+  )
+}
--- app/(ui)/Admin/Component/Cards.tsx
+++ app/(ui)/Admin/Component/Cards.tsx
@@ -1,34 +1,7 @@
 import React from 'react'
-import Card from './Card'
-import { countPendingProjects,countAllProjects,countReviewedProjects } from '@/app/lib/actions'
-import { getServerSession } from 'next-auth';
-import { redirect } from 'next/navigation';
 
-
-export default async function Cards() {
-  const session = await getServerSession()
-  if(!session){
-    redirect('/')
-  }
-  
-  const pending = await countPendingProjects()
-  const total = await countAllProjects()
-  const reviewed = await countReviewedProjects()
-
-
+export default function Cards() {
   return (
-    <>
-    <div className='w-full h-48 flex gap-3 justify-around items-center'>
-        <Card title='Total' number={total || 0}></Card>
-        <Card title='Pending' number={pending || 0}></Card>
-        <Card title='Reviewed' number={reviewed || 0}></Card>
-    </div>
-    </>
+    <div>Cards</div>
   )
 }
-
-
-
-
-
-
