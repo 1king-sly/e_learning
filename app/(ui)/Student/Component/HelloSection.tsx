42318072a5cb0ff503a86b299266344abc07f40c
Byrone Kinsly <Kinslybyrone17@gmail.com>
2024-02-03 23:34:30 +0300

    user credentials now visible at session

--- app/(ui)/Student/Component/HelloSection.tsx
+++ app/(ui)/Student/Component/HelloSection.tsx
@@ -9,7 +9,7 @@ function HelloSection() {
     <div className='p-10 m-20 flex bg-gray-100 rounded-3xl w-1/2'>
         <div className='font-serif'>
         <h1 className='text-2xl'>Greetings,</h1>
-        <h1 className='text-3xl'>{user.data?.firstName}</h1>
+        <h1 className='text-3xl'>{user.data?.firstName || 'User'}</h1>
         </div>
         <div className='ml-auto mr-20 bg-red-500'>
             <h1>Representation</h1>
