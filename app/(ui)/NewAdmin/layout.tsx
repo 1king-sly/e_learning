d9f5bd538946ce3b3a59f0d524489f1b39cab071
ClarenceLaria <clarencelaria@gmail.com>
2024-02-08 16:02:55 +0300

    Cluster and Create Pages

--- app/(ui)/NewAdmin/layout.tsx
+++ app/(ui)/NewAdmin/layout.tsx
@@ -13,8 +13,8 @@ export default async function layout({
             <div className='w-full bg-gray-200 shadow-md h-[10vh]'>
                 <NavBar></NavBar>
             </div>
-            <div className='w-full  max-h-full h-full flex flex-row'>
-                <div className='h-full overflow-hidden w-[15vw] items-center justify-center bg-sky-500  flex' >
+            <div className='w-full  max-h-full h-full flex flex-row '>
+                <div className='h-full overflow-hidden w-[15vw] items-center justify-center bg-sky-500 hidden sm:block' >
                 <SideNav></SideNav>
             </div>
             <div className='h-full w-full bg-gray-200 overflow-y-scroll'>{children}
