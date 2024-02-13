c24e6a303693a809482e2f014b860c59378becbf
ClarenceLaria <clarencelaria@gmail.com>
2024-02-13 18:52:51 +0300

    AddStudent and AddTeacher

--- app/(ui)/NewAdmin/(pages)/Teachers/page.tsx
+++ app/(ui)/NewAdmin/(pages)/Teachers/page.tsx
@@ -5,7 +5,7 @@ import { fetchTeachers } from '@/app/lib/actions';
 import ProfilePic from '@/public/images/ProfilePic.jpeg'
 import Image from 'next/image'
 import Link from 'next/link';
-
+import AddTeacher from '../../Components/AddTeacher';
 
 export default async function TeacherPage({searchParams}:{searchParams:string}) {
 
@@ -13,15 +13,16 @@ export default async function TeacherPage({searchParams}:{searchParams:string}) 
   const q = params.get('query') || '';
   const teachers = await fetchTeachers(q)
   return (
-    <>
-        <div className='flex flex-row justify-between'>
+    <>  
+        <AddTeacher></AddTeacher>
+        {/* <div className='flex flex-row justify-between'>
             <div className='mx-20 mt-10'>
                 <h1 className='text-4xl font-serif font-bold'>Teachers</h1>
             </div>
             <div className='mt-10 mx-20 cursor-pointer'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 24 24"><path fill="currentColor" d="M11.5 12.5H6v-1h5.5V6h1v5.5H18v1h-5.5V18h-1z"/></svg>
             </div>
-        </div>
+        </div> */}
         
         <SearchBar placeholder='search'></SearchBar>
         
