bbd7bee87732995d79152d36eaf49e619f974c94
Byrone Kinsly <Kinslybyrone17@gmail.com>
2024-02-15 12:37:20 +0300

    optimized teachers page

--- app/(ui)/Teacher/(pages)/Assignment/page.tsx
+++ app/(ui)/Teacher/(pages)/Assignment/page.tsx
@@ -1,33 +1,33 @@
 'use server'
 import React from 'react'
 import { FolderIcon } from '@heroicons/react/24/outline'
-import { fetchStudentExamClusters } from '@/app/lib/actions'
+import { fetchStudentAssignmentClusters } from '@/app/lib/actions'
 import Link from 'next/link'
 import SearchBar from '@/app/(ui)/Student/Component/SearchBar'
 
-export default async function ExamsPage({searchParams}:{searchParams:string}) {
+
+export default async function AssignmentPage({searchParams}:{searchParams:string}) {
 
   const params = new URLSearchParams(searchParams);
   const q = params.get('query') || '';
-  const exams = await fetchStudentExamClusters(q)
+  const exams = await fetchStudentAssignmentClusters(q)
   return (
     <>
         <div>
             <div className='mx-20 mt-10'>
-                <h1 className='text-4xl font-serif font-bold'>Exams</h1>
+                <h1 className='text-4xl font-serif font-bold'>Assignments</h1>
             </div>
             <SearchBar placeholder='Search'></SearchBar>
 
          <div>
         <div className='pl-20 grid grid-cols-4 gap-4 p-5'>
          { exams?.map((exam)=>(
-          <Link href={`/Student/Exams/${exam.id}` } key={exam.id}>
-             <div className='' key={exam.id}>
-            <FolderIcon className='h-5 w-5 md:h-6 md:w-6 lg:w-40 lg:h-40'></FolderIcon>
-            <h1 className='ml-8 text-lg '>{exam.title} </h1>
-            </div>
-          </Link>
-           
+            <Link href={`/Student/Assignment/${exam.id}` } key={exam.id}>
+            <div className='' key={exam.id}>
+           <FolderIcon className='h-5 w-5 md:h-6 md:w-6 lg:w-40 lg:h-40'></FolderIcon>
+           <h1 className='ml-8 text-lg '>{exam.title} </h1>
+           </div>
+         </Link>
          ))}   
         </div>
     </div>
--- app/(ui)/Teacher/(pages)/View/[id]/not-found.tsx
+++ app/(ui)/Teacher/(pages)/View/[id]/not-found.tsx
@@ -7,9 +7,9 @@ export default function NotFound() {
     <div className='w-full h-full flex flex-col justify-center items-center gap-2'>
     <FaceFrownIcon className="ml-1 w-20 text-gray-500" />
     <h2>404 Not Found</h2>
-    <p>Could Not Find Requested Exam</p>
+    <p>Could Not Find Requested Assignment</p>
 
-    <Link href='/Student/Dashboard'>
+    <Link href='/Student/Assignment'>
         <button className='p-3 text-white bg-sky-300 rounded-md'>Go Back</button>
     </Link>
 
