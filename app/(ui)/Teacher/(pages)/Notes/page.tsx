96df9bd8b5206013b0a7c82fa533a76aa99caafc
Byrone Kinsly <Kinslybyrone17@gmail.com>
2024-02-19 10:57:44 +0300

    updated teachers page

---
+++ app/(ui)/Teacher/(pages)/Notes/page.tsx
@@ -0,0 +1,37 @@
+'use server'
+import React from 'react'
+import { FolderIcon } from '@heroicons/react/24/outline'
+import { fetchStudentBookClusters } from '@/app/lib/actions'
+import Link from 'next/link'
+import SearchBar from '@/app/(ui)/Teacher/Component/SearchBar'
+
+
+export default async function NotesPage({searchParams}:{searchParams:string}) {
+
+  const params = new URLSearchParams(searchParams);
+  const q = params.get('query') || '';
+  const exams = await fetchStudentBookClusters(q)
+  return (
+    <>
+        <div>
+            <div className='mx-20 mt-10'>
+                <h1 className='text-4xl font-serif font-bold'>Notes</h1>
+            </div>
+            <SearchBar placeholder='Search'></SearchBar>
+
+         <div>
+        <div className='pl-20 grid grid-cols-4 gap-4 p-5'>
+         { exams?.map((exam)=>(
+             <Link href={`/Teacher/Notes/${exam.id}` } key={exam.id}>
+             <div className='' key={exam.id}>
+            <FolderIcon className='h-5 w-5 md:h-6 md:w-6 lg:w-40 lg:h-40'></FolderIcon>
+            <h1 className='ml-8 text-lg '>{exam.title} </h1>
+            </div>
+          </Link>
+         ))}   
+        </div>
+    </div>
+        </div>
+    </>
+  )
+}
--- app/(ui)/Teacher/Component/SearchBar.tsx
+++ app/(ui)/Teacher/Component/SearchBar.tsx
@@ -1,35 +1,47 @@
-'use client'
-import React from 'react'
+import React, { Suspense } from 'react'
 import { MagnifyingGlassIcon } from '@heroicons/react/16/solid'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { useDebouncedCallback } from 'use-debounce'
 
-export default function SearchBar({placeholder}:{placeholder:string}) {
+function Search() {
+  const searchParams = useSearchParams()
 
   const pathname = usePathname()
-    const searchParams = useSearchParams()
-    const {replace} = useRouter()
+  const { replace } = useRouter()
 
-    const handleSearch = useDebouncedCallback( (term:string) =>{
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params = new URLSearchParams(searchParams)
     
-      const params = new URLSearchParams(searchParams)
-      
-       if(term){
-        params.set('query',term)
-       }
-       else{
-        params.delete('query')
-       }
-  
-       replace(`${pathname}?${params.toString()}`)
-  
-      },200)
+    if (term) {
+      params.set('query', term)
+    } else {
+      params.delete('query')
+    }
+
+    replace(`${pathname}?${params.toString()}`)
+
+  }, 200)
+
   return (
     <div className='relative mx-20 my-5 '>
-<input type="text"  name='searchBox'  placeholder={placeholder} className='w-full p-2 border-2 rounded-lg pr-10 outline-sky-300'   onChange={(e)=>{
-        handleSearch(e.target.value)
-      }}/>
-    <MagnifyingGlassIcon className='h-5 w-5 md:h-6 md:w-6 lg:w-7 lg:h-7 absolute top-2 right-2 cursor-pointer'></MagnifyingGlassIcon>
+      <input
+        type="text"
+        name='searchBox'
+        placeholder="Search..."
+        className='w-full p-2 border-2 rounded-lg pr-10 outline-sky-300'
+        onChange={(e) => {
+          handleSearch(e.target.value)
+        }}
+      />
+      <MagnifyingGlassIcon className='h-5 w-5 md:h-6 md:w-6 lg:w-7 lg:h-7 absolute top-2 right-2 cursor-pointer' />
     </div>
+  )
+}
+
+export default function SearchBar({ placeholder }: { placeholder: string }) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Search />
+    </Suspense>
   )
 }
